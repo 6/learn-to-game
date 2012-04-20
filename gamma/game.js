cf514d4713aea79f1b5fbd70d2f40477f9acf066
Peter Graham <pg.sners@gmail.com>
2012-04-20 14:05:41 -0400

    keyboard movement

--- gamma/game.js
+++ gamma/game.js
@@ -15,9 +15,13 @@ require(['gma/base', 'gma/manager', 'gma/entities/character', 'gma/events'],
                 {top:10, left:2, width:20, height:3}
             ]
         };
+        manager.storeLevels(myLevel);
+        
+        gma.keyHandler.register(37, manager.character.move.curry(gma.constants.LEFT));
+        gma.keyHandler.register(39, manager.character.move.curry(gma.constants.RIGHT));
+        gma.keyHandler.register(32, manager.character.jump);
         
         // intiate the game loop: http://gammajs.org/glossary.html#term-game-loop
-        manager.storeLevels(myLevel);
         manager.init();
     }
 );
