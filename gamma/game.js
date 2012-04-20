4fc5f1c47ef5911c23ba05f50b15729e4281800a
Peter Graham <pg.sners@gmail.com>
2012-04-20 13:53:25 -0400

    set w & h explicitly

--- gamma/game.js
+++ gamma/game.js
@@ -2,7 +2,7 @@ require(['gma/base', 'gma/manager'],
 
     function(gma) {
         // The game specification is contained within this function.
-        var manager = gma.manager();
+        var manager = gma.manager({width: 600,height: 500});
         manager.storeLevels({});
         manager.init();
     }
