5b83e199a9eedb722ca792f79a539efcb605db8d
Peter Graham <pg.sners@gmail.com>
2012-04-20 13:54:35 -0400

    add comment

--- gamma/game.js
+++ gamma/game.js
@@ -3,6 +3,7 @@ require(['gma/base', 'gma/manager'],
     function(gma) {
         // The game specification is contained within this function.
         var manager = gma.manager({width: 600,height: 500});
+        // intiate the game loop: http://gammajs.org/glossary.html#term-game-loop
         manager.storeLevels({});
         manager.init();
     }
