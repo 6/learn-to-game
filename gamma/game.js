86318983531accd39042c24eb93f29411d2c045c
Peter Graham <pg.sners@gmail.com>
2012-04-20 13:59:13 -0400

    add some platforms

--- gamma/game.js
+++ gamma/game.js
@@ -3,8 +3,16 @@ require(['gma/base', 'gma/manager'],
     function(gma) {
         // The game specification is contained within this function.
         var manager = gma.manager({width: 600,height: 500});
+        
+        var myLevel = {
+            entities : [
+                {top:0, left:-7, width:9, height:1},
+                {top:10, left:2, width:20, height:3}
+            ]
+        };
+        
         // intiate the game loop: http://gammajs.org/glossary.html#term-game-loop
-        manager.storeLevels({});
+        manager.storeLevels(myLevel);
         manager.init();
     }
 );
